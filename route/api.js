224ec30908df21dea5d0eca08173083afb017091
luciferx48 <laukik.22010776@gmail.com>
2022-09-11 01:18:10 +0530

    index js modifed

--- route/api.js
+++ route/api.js
@@ -1,14 +1,17 @@
 var express = require('express');
-
+const timestamp = require('../timestamp');
 const route = express.Router();
 
+route.get('/', (req, res)=>{
+  const d = Date.now();
+  res.send({
+    "unix":d,
+    "utc": new Date(d).toUTCString()
+  });
+})
+
 route.get('/:time', (req, res)=>{
-    const d = new Date();
-    const obj = {
-      "unix":Date.now(),
-      "utc":d.toUTCString()
-  }
-    res.send(obj);
+    res.send(timestamp(req.params.time));
 })
 
 module.exports = route;
